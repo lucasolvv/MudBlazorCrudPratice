bdaef1203e35b835c2756e6efd409eef2b5a4a43
Lucas <lvksounds@gmail.com>
2025-07-22 16:23:28 -0300

    feat(app): ✨ reorganização da estrutura e migrações
    * Removida a linha `<HeadOutlet @rendermode="InteractiveServer" />` do arquivo `App.razor` e adicionada uma nova estrutura de `<head>` e `<body>`, melhorando a organização do HTML gerado.
    * Atualizado o arquivo `20250722190404_InitialMigration.Designer.cs` com novas diretivas `using` e a definição da classe `InitialMigration`, configurando o modelo da entidade `Employee`.
    * Implementada a criação da tabela `Employees` no arquivo `20250722190404_InitialMigration.cs`, definindo suas colunas e tipos, além dos métodos `Up` e `Down` para gerenciar a migração.
    * Modificado o arquivo `AppDbContextModelSnapshot.cs` para incluir a definição do modelo da tabela `Employees`, garantindo a sincronização com o banco de dados.
    * Corrigida a string de conexão no `appsettings.json`, alterando a barra invertida simples (`\`) para uma barra invertida dupla (`\\`) para evitar erros de escape na conexão com o SQL Server.

---
+++ Migrations/20250722190404_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MUDCRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
